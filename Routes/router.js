e330cd59caa9d003651cd8b9d2001c00fb3e8276
mahesh548 <mkmjnp5@gmail.com>
2024-12-22 20:09:22 +0530

    Added: help comments

--- Routes/router.js
+++ Routes/router.js
@@ -13,15 +13,34 @@ const getLyrics = require("./controller/getLyrics");
 
 const router = express.Router();
 
+// query: "lang" array e.g [Hindi,English]
 router.get("/api/home", getHome);
+
+// query: "id" artist id
 router.get("/api/artist", getArtist);
+
+// query: "id" song id
 router.get("/api/song", getSong);
+
+// query: "q" search term
 router.get("/api/search", getSearch);
+
+// query: "entityType" playlist,song,album & "id"
 router.get("/api/related", getRelated);
+
+// query: "entityType" playlist,album & "lang"
 router.get("/api/trending", getTrending);
+
+// query: "entityIds" array of song IDs
 router.get("/api/queue", getQueue);
+
+// query: "entity" featured,artist & "name" radio,artist & "lang"
 router.get("/api/radio", getRadio);
+
+// query: "id" of song
 router.get("/api/lyrics", getLyrics);
 
+// para: "entity" playlist,album,mix & query: "id"
 router.get("/api/entity/:entity", getEntity);
+
 module.exports = router;

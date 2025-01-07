4ffeebb5943605fafa79268bce889837210a2fd6
mahesh548 <mkmjnp5@gmail.com>
2025-01-07 11:44:09 +0530

    Updated: Auth system

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
+require("dotenv").config();
 const express = require("express");
 const router = require("./Routes/router");
 const auth = require("./Routes/Middlewares/authentication");
 const connectDB = require("./Database/connect")();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+//cors setting
+const frontendUrl = process.env.FURL;
+const corsOptions = {
+  origin: frontendUrl,
+  methods: ["GET", "POST"],
+};
 
 const app = express();
-app.use(router);
-app.use(auth);
+
+//Middlewares
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(express.json());
+
+//authentication middleware
+app.use(auth);
+
+//router
+app.use(router);
+
 app.listen(5000, () => {
   console.log("API listening on port 5000...");
 });

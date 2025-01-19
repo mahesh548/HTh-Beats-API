a65ffbecfd2cda9cc6011e5043da834bb5ef2a53
mahesh548 <mkmjnp5@gmail.com>
2025-01-19 17:18:00 +0530

    update: simplify getHome controller logic and remove language check

--- Routes/controller/getHome.js
+++ Routes/controller/getHome.js
@@ -1,28 +1,20 @@
 const home = require("../../Database/Models/Home");
-const { checkLanguage, api, dura } = require("../../utils");
+const { api, dura } = require("../../utils");
 const getHome = async (req, res) => {
-  if (!checkLanguage(req.query.lang))
-    return res.status(400).json({ status: "language missing!" });
-
-  const langArray = req.query.lang.split(",").sort();
-
   try {
-    const homeData = await home.findOne({ language: langArray }, ["-_id"]);
+    const homeData = await home.findOne({ createdAt: { $exists: true } });
     if (homeData && dura(homeData.createdAt).hrs < 24) {
       return res.status(200).json(homeData);
     }
 
     const data = await api(
-      "https://www.jiosaavn.com/api.php?__call=webapi.getLaunchData&api_version=4&_format=json&_marker=0&ctx=web6dot0",
-      { cookie: `L=${req.query.lang}` }
+      "https://www.jiosaavn.com/api.php?__call=webapi.getLaunchData&api_version=4&_format=json&_marker=0&ctx=web6dot0"
     );
-    console.log(Object.keys(data.data));
 
     if (!data.status) return res.status(500).json({ status: "api error" });
 
-    if (homeData) await home.deleteOne({ _id: homeData._id });
+    if (homeData) await home.deleteOne({ createdAt: { $exists: true } });
     const newData = await new home({
-      language: langArray,
       data: data.data,
     }).save();
 

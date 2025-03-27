2964305a2122fc8349cab3343e6a71766cd30c6a
Mahesh <mkmjnp5@gmail.com>
2025-03-27 15:54:36 +0530

    update: add artist search functionality and integrate with existing search logic

--- Routes/controller/manageSearch.js
+++ Routes/controller/manageSearch.js
@@ -2,6 +2,7 @@ const { api, mergeOnIds, uniqueItemFromArray } = require("../../utils");
 const Song = require("../../Database/Models/Song");
 const Search = require("../../Database/Models/Search");
 const Entity = require("../../Database/Models/Entity");
+const Artist = require("../../Database/Models/Artist");
 const searchRecord = require("../../Database/Models/Record");
 
 const searchGet = async (q, autocomplete) => {
@@ -38,22 +39,26 @@ const addSearch = async (list) => {
 const searchQuery = async (q, autocomplete) => {
   const searching = await searchSearch(q); //searching for Search collection
   const songs = await searchSongs(q); //searching complete songs collection
-  const entity = await searchEntity(q);
+  const entity = await searchEntity(q); //searching entity collection
+  const artist = await searchArtist(q); // searching artist collection
 
-  const searchData = uniqueItemFromArray([searching, songs, entity]); //merging all data
+  const searchData = uniqueItemFromArray([searching, songs, entity, artist]); //merging all data
+
+  const record = await searchRecord.findQuerySound(q); // if result not enough search if search term is already called to api
 
   if (searchData.length >= 20) return { status: true, data: searchData }; //return data to user if result is enough
 
-  const record = await searchRecord.findQuerySound(q); // if result not enough search if search term is already called to api
   if (record.length != 0) {
+    //if already searched by api then get item by their specific id
     const specificSearch = await Search.find({
       id: { $in: record },
-    }); //if already searched by api then get item by their specific id
+    });
+
     const mergedData = uniqueItemFromArray(searchData, specificSearch);
-    return { status: true, data: mergedData }; //merging data and sending back
+    return { status: true, data: mergedData, record: record }; //merging data and sending back
   }
 
-  if (autocomplete == "true") return { status: true, data: searchData };
+  if (autocomplete == "true") return { status: true, data: searchData }; // if only autocomplete then don't call api
 
   const data = await api(
     `https://www.jiosaavn.com/api.php?__call=autocomplete.get&_format=json&_marker=0&cc=in&includeMetaTags=1&query=${q}`
@@ -127,6 +132,26 @@ const searchEntity = async (q) => {
         ],
       },
       ["title", "subtitle", "type", "image", "perma_url", "id"]
+    ).limit(10);
+
+    return data;
+  } catch (error) {
+    console.log(error.message);
+    return [];
+  }
+};
+
+const searchArtist = async (q) => {
+  try {
+    const data = await Artist.find(
+      {
+        $and: [
+          {
+            $or: [{ name: { $regex: `\\b${q}`, $options: "i" } }],
+          },
+        ],
+      },
+      ["name", "type", "image", "perma_url", "id"]
     ).limit(10);
 
     return data;

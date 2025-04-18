feaae678e2fd866b8f898f886be4c8c72c88ea53
Mahesh <mkmjnp5@gmail.com>
2025-04-19 00:08:02 +0530

    update: refactor getQueue function to improve entity ID handling and response structure

--- Routes/controller/getQueue.js
+++ Routes/controller/getQueue.js
@@ -1,7 +1,6 @@
 const { api, objToArr, dura } = require("../../utils");
 const Song = require("../../Database/Models/Song");
-const { addSongs } = require("./manageSongs");
-const { addSearch } = require("./manageSearch");
+const { addSongs, getSongs } = require("./manageSongs");
 const Queue = require("../../Database/Models/Queue");
 const getUrl = (type, ids) => {
   const urls = {
@@ -18,33 +17,37 @@ const getUrl = (type, ids) => {
   return urls[type][0] + JSON.stringify(ids) + urls[type][1];
 };
 const getQueue = async (req, res) => {
-  let entityIds = req?.query?.entityIds;
+  let entityIds = req.body?.ids;
+  const user = req.body.user;
 
-  if (entityIds?.length == 0)
+  if (entityIds?.length == 0 || !Array.isArray(entityIds))
     return res
       .status(400)
       .json({ status: false, msg: "Entity IDs are missing!" });
   try {
-    entityIds = JSON.parse(entityIds);
-    const queueFind = await Queue.findOne({ station: entityIds });
+    const queueFind = await Queue.findOne({ station: entityIds.sort() });
     if (queueFind && dura(queueFind.createdAt).hrs < 160) {
-      const respondData = await Song.find({ id: { $in: queueFind.ids } });
-      return res.status(200).json(respondData);
+      const respondData = await getSongs(entityIds, user.id);
+      return res.status(200).json({ status: true, data: respondData });
     }
     const data = await api(getUrl("id", entityIds));
-    if (!data.status) return res.status(500).json({ status: "api error" });
+    if (!data.status)
+      return res.status(200).json({ status: false, msg: "api error" });
     if (!data?.data?.stationid)
-      return res.status(500).json({ status: "api error" });
+      return res.status(200).json({ status: false, msg: "api error" });
 
     const queueData = await api(getUrl("queue", data.data.stationid));
-    if (!queueData.status) return res.status(500).json({ status: "api error" });
+    if (!queueData.status)
+      return res.status(200).json({ status: false, msg: "api error" });
     if (queueFind) await Queue.deleteOne({ _id: queueFind._id });
     const arr = objToArr(queueData.data);
 
     const songIds = await addSongs(arr);
-    await addSearch(arr);
-    await new Queue({ station: entityIds, ids: songIds }).save();
-    res.status(200).json(arr);
+    await new Queue({ station: entityIds.sort(), ids: songIds }).save();
+
+    const responseData = await getSongs(songIds, user.id);
+
+    res.status(200).json({ status: true, data: responseData });
   } catch (error) {
     res.status(500).json({ status: false, msg: error.message });
   }

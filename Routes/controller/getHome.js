a0afa1d038a5c57c0532f5c3e941103b949b1cb1
Mahesh <mkmjnp5@gmail.com>
2025-05-25 23:33:24 +0530

    update: change context from wap6dot0 to web6dot0 in API URLs for consistency

--- Routes/controller/getHome.js
+++ Routes/controller/getHome.js
@@ -20,7 +20,7 @@ const getHome = async (req, res) => {
     }
 
     const data = await api(
-      "https://www.jiosaavn.com/api.php?__call=webapi.getLaunchData&api_version=4&_format=json&_marker=0&ctx=wap6dot0",
+      "https://www.jiosaavn.com/api.php?__call=webapi.getLaunchData&api_version=4&_format=json&_marker=0&ctx=web6dot0",
       {
         Cookie: `L=${langString};`,
       }
